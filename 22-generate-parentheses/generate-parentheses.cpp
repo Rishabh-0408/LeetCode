ea31926c9e0ec11574f1d83a56c5ba0a60246711
Rishabh kumar sahu <101442744+Rishabh-0408@users.noreply.github.com>
2026-02-12 10:15:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (94.64%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
-    void Back_track(string curr,int n,int open,int close,vector<string>& Str){
-        if(curr.length() == 2*n){
-            Str.push_back(curr);
-            return;
-        }
-        if(open<n) Back_track(curr+'(',n,open+1,close,Str);
-        if(close<open) Back_track(curr+')',n,open,close+1,Str);
+    void Back_track(string &curr, int n, int open, int close, vector<string>& Str) {
+    if (curr.length() == 2 * n) {
+        Str.push_back(curr);
+        return;
     }
+    if (open < n) {
+        curr.push_back('(');        // DO (Action)
+        Back_track(curr, n, open + 1, close, Str);
+        curr.pop_back();            // UNDO (Backtrack)
+    }
+    if (close < open) {
+        curr.push_back(')');        // DO
+        Back_track(curr, n, open, close + 1, Str);
+        curr.pop_back();            // UNDO
+    }
+}
     vector<string> generateParenthesis(int n) {
         vector<string> Str;
-        Back_track("", n, 0, 0, Str);
+        string curr = "";
+        Back_track(curr, n, 0, 0, Str);
         return Str;
     }
 };

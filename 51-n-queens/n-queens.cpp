333a654fc1b2a882b0f6300b362336e808a853ae
Rishabh kumar sahu <101442744+Rishabh-0408@users.noreply.github.com>
2026-02-19 13:06:14 +0530

    Time: 3 ms (62.73%) | Memory: 10.1 MB (76.26%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,43 +1,34 @@
 class Solution {
 public:
-    bool isSafe(int col,int row, vector<string>& board, int n){
-        for(int j=0; j<col; j++){
-            //checking each col of the same row 
-            if(board[row][j] == 'Q'){
-                return false;
-            }
-        }
-        for(int i=row,j=col;i>=0 && j>=0;i--,j--){
-            //checking for the upper diagonal
-            if(board[i][j] == 'Q'){
-                return false;
-            }
-        }
-        for(int i=row,j=col;i<n && j>=0;i++,j--){
-            //checking for the lower diagonal
-            if(board[i][j] == 'Q'){
-                return false;
-            }
-        }
-        return true;
-    }
-    void toCheck(int col,int n, vector<string>& board,vector<vector<string>>& ans){
+    void toCheck(int col,int n, vector<string>& board,vector<vector<string>>& ans, vector<int>& leftrows,vector<int>& upperDiagonal, vector<int>& lowerDiagonal){
         if(col==n){
             ans.push_back(board);
             return;
         }
-        for(int row=0; row<n; row++){
-            if(isSafe(col,row,board,n)){
-                board[row][col] = 'Q';
-                toCheck(col+1, n, board, ans);
-                board[row][col] = '.';
-            }
+       for(int row = 0; row < n; row++){
+        if(leftrows[row] == 0 && upperDiagonal[n-1+col-row] == 0 && lowerDiagonal[row+col] == 0){
+            board[row][col] = 'Q';
+            leftrows[row] = 1;
+            upperDiagonal[n-1+col-row] = 1;
+            lowerDiagonal[row+col] = 1;
+
+            toCheck(col+1,n,board,ans,leftrows,upperDiagonal,lowerDiagonal);
+
+            board[row][col] = '.';
+            leftrows[row] = 0;
+            upperDiagonal[n-1+col-row] = 0;
+            lowerDiagonal[row+col] = 0;
+
         }
+       }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
         vector<string> board(n, string(n,'.'));
-        toCheck(0,n,board,ans);
+        vector<int> leftrows(n,0);
+        vector<int> lowerDiagonal(2*n-1,0);
+        vector<int> upperDiagonal(2*n-1,0);
+        toCheck(0,n,board,ans,leftrows,upperDiagonal,lowerDiagonal);
         return ans;
     }
 };

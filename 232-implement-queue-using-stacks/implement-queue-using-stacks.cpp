824258ec9fe82ee0ec470731dd19e3e9ed045d48
Rishabh kumar sahu <101442744+Rishabh-0408@users.noreply.github.com>
2026-03-09 11:22:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (69.53%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -7,34 +7,33 @@ public:
     }
     
     void push(int x) {
-        //Step 1: Move all elements from s1 to s2
-        while(!st1.empty()){
-            st2.push(st1.top());
-            st1.pop();
-        }
-
-        //Step 2: Push the new element into s1
         st1.push(x);
-
-        //Step 3: Move all elements from s2 to s1
-        while(!st2.empty()){
-            st1.push(st2.top());
-            st2.pop();
-        }
     }
     
     int pop() {
-        int top_val = st1.top();
-        st1.pop();
+        if(st2.empty()){
+            while(!st1.empty()){
+                st2.push(st1.top());
+                st1.pop();
+            }
+        }
+        int top_val = st2.top();
+        st2.pop();
         return top_val;
     }
     
     int peek() {
-        return st1.top();
+        if(st2.empty()){
+            while(!st1.empty()){
+                st2.push(st1.top());
+                st1.pop();
+            }
+        }
+        return st2.top();
     }
     
     bool empty() {
-        return st1.empty();
+        return st1.empty() && st2.empty();
     }
 };
 

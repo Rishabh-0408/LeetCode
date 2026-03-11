7382e78d3cad9c6045ce2b7639e69eb3c739b1b3
Rishabh kumar sahu <101442744+Rishabh-0408@users.noreply.github.com>
2026-03-11 15:43:41 +0530

    Time: 2 ms (20.30%) | Memory: 27 MB (39.74%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,31 +2,29 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
+        stack<int> st;
         int totalWater = 0;
-        int leftPtr = 0, rightPtr = n-1;
-        int leftMax = 0, rightMax = 0;
-        while(leftPtr <= rightPtr){
-            if(height[leftPtr] <= height[rightPtr]){
-                if(height[leftPtr] > leftMax){
-                    leftMax = height[leftPtr];
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && height[st.top()] < height[i]){
+                // 1. BASE NIKALO (Gaddha)
+                int base = st.top();
+                st.pop();
+
+                if(st.empty()){
+                    break;
                 }
-                else{
-                    totalWater += leftMax - height[leftPtr];
-                }
-                leftPtr++;
+
+                int leftPtr = st.top();
+
+                int width = i - leftPtr - 1;
+
+                int boundary_height = min(height[i],height[leftPtr]) - height[base];
+
+                totalWater += width * boundary_height;
             }
-            else{
-               if(height[rightPtr] <= height[leftPtr]){
-                    if(height[rightPtr] > rightMax){
-                        rightMax = height[rightPtr];
-                    }
-                    else{
-                        totalWater += rightMax - height[rightPtr]; 
-                    }
-                    rightPtr--;
-                }
-            }
-        }
+            // Current building ko stack mein daal do aage ke calculations ke liye
+            st.push(i);
+        } 
         return totalWater;
     }
 };

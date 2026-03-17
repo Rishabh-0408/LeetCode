144e9d2310ab1a0dd420d48181a5b88b38547f74
Rishabh kumar sahu <101442744+Rishabh-0408@users.noreply.github.com>
2026-03-17 10:03:55 +0530

    Time: 1 ms (76.15%) | Memory: 11.2 MB (84.23%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        string res = "";
+        for(char c : num){
+            while(!res.empty() && res.back() > c && k > 0)
+            {
+                res.pop_back();
+                k--;
+            }
+            if(!res.empty() || c != '0')
+            {
+                res.push_back(c);
+            }
+        }
+        // Edge Case: If the array was completely monotonically increasing (e.g., "12345" and k=2)
+        // We still have k > 0, so remove from the end (the largest numbers)
+        while (!res.empty() && k > 0) {
+            res.pop_back();
+            k--;
+        }
+
+        return res.empty() ? "0" : res;
+    }
+};

4457977aaacbac3bfad27e6c70aeb0ee185ada78
Rishabh kumar sahu <101442744+Rishabh-0408@users.noreply.github.com>
2026-04-20 12:38:52 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (35.15%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0;
-        for(int i=0; i<32; i++){
-            int count = 0;
-            for(auto num:nums){
-                if(num & (1<<i)){
-                    count++;
-                }
-            }
-            if(count % 3 == 1){
-                ans = ans | (1<<i);
-            }
+        int one = 0;
+        int two = 0;
+        for(auto num : nums){
+            one = (one ^ num) & ~two;
+            two = (two ^ num) & ~one;
         }
-        return ans;
+        return one;
     }
 };
